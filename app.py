dc744ebe4a3ff40c63cee5d41e2c72c297a0e62b
baptiste-bedouret <b.bedouret@gmail.com>
2024-12-14 15:45:47 +0100

    hide landmarks also for the app

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def generate():
             break
         else:
             # Detect pose
-            image = pose_detector.findPose(image)
+            image = pose_detector.findPose(image, draw=False)
             pose_landmarks = pose_detector.findPosition(image)
 
             # Check if a dab is detected
@@ -57,7 +57,7 @@ def generate():
                 last_dab_time = None
 
             # Detect hands
-            image = hand_detector.findHands(image)
+            image = hand_detector.findHands(image, draw=False)
             hand_landmarks = []
             if hand_detector.results.multi_hand_landmarks:
                 for hand_no in range(len(hand_detector.results.multi_hand_landmarks)):
